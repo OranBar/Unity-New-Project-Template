da9f71b59cd9bb070320ffb33ea11d6c240df88a
Oran <baroran@hotmail.com>
2018-01-08 02:52:41 +0100

    Adding scripts

---
+++ Assets/Programming/Oran/Scripts/ExtensionMethods/IEnumerableEx.cs
@@ -0,0 +1,25 @@
+﻿/*======= Copyright (c) Immerxive Srl, All rights reserved. ===================
+
+Author: Oran Bar
+
+Purpose: To help with Linq operations. I have enough of writing .ToList().ForEach(), because you can't foreach on a IEnumerable, only on a List. This ends NOW.
+
+Notes:
+
+=============================================================================*/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using OranUnityUtils;
+
+
+public static class IEnumerableEx {
+
+	public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+	{
+		foreach (var item in source)
+			action(item);
+	}
+}
---
+++ Assets/SplineTweener.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Pixelplacement;
+using Pixelplacement.TweenSystem;
+
+public class SplineTweener : MonoBehaviour
+{
+	public bool startOnAwake;
+	public Spline mySpline;
+	public Transform targetTransform;
+	public float duration = 3;
+	public float dealy = 0;
+	public bool faceDirection = true;
+	public Tween.LoopType loopType = Tween.LoopType.PingPong;
+
+	public TweenBase SplineTween { get; private set; }
+
+	void Awake()
+	{
+		if (startOnAwake)
+		{
+			StartSplineTweening();
+		}
+	}
+
+	public void StartSplineTweening()
+	{
+		SplineTween = Tween.Spline(mySpline, targetTransform, 0, 1, faceDirection, duration, dealy, Tween.EaseInOut, loopType);
+	}
+}
