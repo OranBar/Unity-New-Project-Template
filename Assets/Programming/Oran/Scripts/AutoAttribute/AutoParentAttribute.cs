77ba60fd295f069c2861868b1a13a607772d187b
Oran <baroran@hotmail.com>
2018-03-03 03:09:50 +0100

    Refactory!!!

--- Assets/Programming/Oran/Scripts/AutoAttribute/AutoParentAttribute.cs
+++ Assets/Programming/Oran/Scripts/AutoAttribute/AutoParentAttribute.cs
@@ -22,146 +22,15 @@ using System.Reflection;
 using UnityEngine;
 
 [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
-public class AutoParentAttribute : Attribute, IAutoAttribute
+public class AutoParentAttribute : AutoFamily
 {
-	private const string MonoBehaviourNameColor = "green";
-	private static ReflectionHelperMethods Rhm = new ReflectionHelperMethods();
-
-	private bool logErrorIfMissing = true;
-
-	private Component targetComponent;
-
-	public AutoParentAttribute(bool autoAdd)
+	protected override string GetMethodName()
 	{
-		this.logErrorIfMissing = true;
+		return "GetComponentsInParent";
 	}
 
-	public AutoParentAttribute(bool autoAdd = false, bool getMadIfMissing = true)
+	protected override object GetTheSingleComponent(MonoBehaviour mb, Type componentType)
 	{
-		this.logErrorIfMissing = getMadIfMissing;
+		return mb.GetComponentInParent(componentType);
 	}
-
-	public void Execute(MonoBehaviour mb, Type componentType, Action<MonoBehaviour, object> SetVariableType)
-	{
-		GameObject go = mb.gameObject;
-
-		if (componentType.IsArray || Rhm.IsList(componentType))
-		{
-			MultipleComponentAssignment(mb, go, componentType, SetVariableType);
-		}
-		else
-		{
-			SetVariableType(mb, mb.GetComponentsInParent(componentType, true));
-		}
-	}
-
-	private void MultipleComponentAssignment(MonoBehaviour mb, GameObject go, Type componentType, Action<MonoBehaviour, object> SetVariable)
-	{
-		Type listElementType = AutoUtils.GetElementType(componentType);
-
-		MethodInfo method = typeof(GameObject).GetMethods()
-			.First(m =>
-			{
-				bool result = true;
-				result = result && m.Name == "GetComponentsInParent";
-				result = result && m.IsGenericMethod;
-				result = result && m.GetParameters().Length == 1;
-				result = result && m.GetParameters()[0].ParameterType == typeof(bool);
-				return result;
-			});
-		//we want to pass true as arg, to get from inactive objs too
-		MethodInfo generic = method.MakeGenericMethod(listElementType);
-		dynamic componentsToReference = generic.Invoke(go, new object[] { true });
-
-		if (componentsToReference.Length == 0)
-		{
-			if (logErrorIfMissing)
-			{
-				Debug.LogError(
-					string.Format("[Auto]: <color={3}><b>{1}</b></color> couldn't find any components <color=#cc3300><b>{0}</b></color> on <color=#e68a00>{2}.</color>",
-						componentType.Name, mb.GetType().Name, go.name, MonoBehaviourNameColor)
-					, go);
-			}
-			return;
-		}
-
-		if (componentType.IsArray)
-		{
-			SetVariable(mb, componentsToReference);
-			//field.SetValue(mb, componentsToReference);
-		}
-		else if (Rhm.IsList(componentType))
-		{
-			SetVariable(mb, componentsToReference.ToList());
-		}
-		//	List<Component> list = new List<Component>(componentsToReference);
-		//	var argTypes = new[]
-		//	{
-		//		(IEnumerable<Component>) componentsToReference
-		//	};
-
-		//	var listType = typeof(List<>);
-		//	var constructedListType = listType.MakeGenericType(componentType);
-
-		//	var componentList = Activator.CreateInstance(constructedListType, argTypes);
-
-		//	field.SetValue(mb, componentList);
-		//}
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	public void Execute(MonoBehaviour mb, FieldInfo field)
-	{
-		GameObject go = mb.gameObject;
-
-		Type componentType = field.FieldType;
-
-		Component componentToReference = go.GetComponentInParent(componentType, true);
-		if (componentToReference == null)
-		{
-			if (logErrorIfMissing)
-			{
-				Debug.LogError(
-					string.Format("[Auto]: <color={3}><b>{1}</b></color> couldn't find <color=#cc3300><b>{0}</b></color> on <color=#e68a00>{2}</color>",
-						componentType.Name, mb.GetType().Name, go.name, MonoBehaviourNameColor)
-					, go);
-			}
-			return;
-		}
-
-		field.SetValue(mb, componentToReference);
-	}
-
-	public void Execute(MonoBehaviour mb, PropertyInfo prop)
-	{
-		GameObject go = mb.gameObject;
-
-		Type componentType = prop.PropertyType;
-
-		Component componentToReference = go.GetComponentInParent(componentType, true);
-		if (componentToReference == null)
-		{
-			if (logErrorIfMissing)
-			{
-				Debug.LogError(string.Format("[Auto] Error: Script {1} couldn't AutoReference component {0} of GameObject {2}",
-					componentType.Name, mb.GetType().Name, go.name), go);
-				return;
-			}
-		}
-
-		prop.SetValue(mb, componentToReference, null);
-	}
-
 }
