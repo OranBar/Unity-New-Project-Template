657eb8c9759c1291482c7daf90572d0b2581f4f4
Oran <baroran@hotmail.com>
2019-06-15 15:32:44 +0200

    initial commit. Test scene + walking physics movement

---
+++ Assets/Programming/Oran/Scripts/CollisionBitch.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CollisionBitch : MonoBehaviour {
+
+    public Action<Collision> OnCollisionEnter_fn = null;
+    public Action<Collision> OnCollisionStay_fn = null; 
+    public Action<Collision> OnCollisionExit_fn = null;
+
+
+    public Action<Collision2D> OnCollisionEnter2D_fn = null;
+    public Action<Collision2D> OnCollisionStay2D_fn = null;
+    public Action<Collision2D> OnCollisionExit2D_fn = null;
+
+    public Action<Collider> OnTriggerEnter_fn = null;
+    public Action<Collider> OnTriggerStay_fn = null;
+    public Action<Collider> OnTriggerExit_fn = null;
+
+    public Action<Collider2D> OnTriggerEnter2D_fn = null;
+    public Action<Collider2D> OnTriggerStay2D_fn = null;
+    public Action<Collider2D> OnTriggerExit2D_fn = null;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        OnCollisionEnter_fn?.Invoke(other);
+    }
+
+	private void OnCollisionExit(Collision other)
+    {
+        OnCollisionExit_fn?.Invoke(other);
+
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        OnCollisionStay_fn?.Invoke(other);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        OnCollisionEnter2D_fn?.Invoke(other);
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        OnCollisionStay2D_fn?.Invoke(other);
+
+    }
+
+	void OnCollisionExit2D(Collision2D other)
+	{
+		OnCollisionExit2D_fn.Invoke(other);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnTriggerEnter_fn?.Invoke(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        OnTriggerExit_fn?.Invoke(other);
+    }
+
+	private void OnTriggerStay(Collider other)
+	{
+        OnTriggerStay_fn?.Invoke(other);
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        OnTriggerEnter2D_fn?.Invoke(other);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        OnTriggerExit2D_fn?.Invoke(other);
+    }
+	
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        OnTriggerStay2D_fn?.Invoke(other);
+    }
+}
---
+++ Assets/Programming/Oran/Scripts/Movement/ODG_Movement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OranUnityUtils;
+
+public class ODG_Movement : MonoBehaviour {
+
+	public Rigidbody main_rb;
+    private bool isTouchingGround;
+    public float speed = 1f;
+	public float max_speed = 100f;
+    public float jumpForce = 1f;
+
+    // Use this for initialization
+    void Start () {
+		var collisionBitch = main_rb.gameObject.AddOrGetComponent<CollisionBitch>();
+		collisionBitch.OnTriggerEnter_fn += On_MainRb_TriggerEnter;
+		collisionBitch.OnTriggerExit_fn += On_MainRb_TriggerExit;
+	}
+	
+	void On_MainRb_TriggerEnter(Collider other){
+		if(other.tag == "Ground"){
+			isTouchingGround = true;
+		} 
+	}
+
+	void On_MainRb_TriggerExit(Collider other){
+		if(other.tag == "Ground"){
+			isTouchingGround = false;
+		} 
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+        Vector3 movement = (this.transform.forward * v) + (this.transform.right * h);
+		movement = movement * speed;
+
+		if(main_rb.velocity.magnitude < max_speed){
+        	// main_rb.AddForce(movement);
+			main_rb.velocity += movement;
+			main_rb.velocity = main_rb.velocity.ClampXZ(-max_speed, max_speed);
+			Debug.Log("Velocity = "+main_rb.velocity.magnitude);
+		}
+
+		if(isTouchingGround){
+			if(Input.GetKeyDown(KeyCode.Space)){
+				Jump();
+			}
+		}
+	}
+
+	void Jump(){
+        main_rb.AddForce(Vector3.up * jumpForce);
+	}
+}
